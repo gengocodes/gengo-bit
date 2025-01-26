7b34efadbdab9198ac10247020f8001a2a89b083
Gengo-bit <paulcorsino28@gmail.com>
2025-01-27 01:30:07 +0800

    add Contact Area

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Projects from "./components/Projects";
 import Header from "./components/Header";
 import Arrow from "./components/Arrow";
 import Welcome from "./components/Welcome";
-import Welcome2 from "./components/Welcome2";
+import Contact from "./components/Contact";
 
 function App() {
   const heroSectionRef = useRef(null);
@@ -14,7 +14,7 @@ function App() {
   const skillsSectionRef = useRef(null);
   const projectsSectionRef = useRef(null);
   const welcomeSectionRef = useRef(null);
-  const welcomeSection2Ref = useRef(null);
+  const contactSectionRef = useRef(null);
 
   return (
     <div className="App">
@@ -30,8 +30,8 @@ function App() {
           <Welcome />
         </section>
 
-        <section ref={welcomeSection2Ref} id="welcome2">
-          <Welcome2 />
+        <section ref={contactSectionRef} id="contact">
+          <Contact />
         </section>
 
         <Arrow />
@@ -40,13 +40,13 @@ function App() {
           <HeroSection />
         </section>
 
-        <section ref={aboutSectionRef} id="about">
+        {/* <section ref={aboutSectionRef} id="about">
           <About />
         </section>
 
         <section ref={skillsSectionRef} id="skills">
           <Skills />
-        </section>
+        </section> */}
 
         <section ref={projectsSectionRef} id="projects">
           <Projects />
