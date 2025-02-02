7dcd805b7b68ed5f687719e29089eb81625d8d92
Gengo-bit <paulcorsino28@gmail.com>
2025-02-02 22:46:28 +0800

    update current project

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
   const [displayedText, setDisplayedText] = useState(""); // Text being displayed
   const [isTyping, setIsTyping] = useState(true); // Whether typing or deleting
   const [currentTextIndex, setCurrentTextIndex] = useState(0); // Index of current text
-  const fullTexts = useRef(["Website!", "Portfolio!"]); // List of texts to alternate
+  const fullTexts = useRef(["Gengo-bit.", "Paul Corsino."]); // List of texts to alternate
   const typingSpeed = 150; // Speed of typing in ms
   const deletingSpeed = 100; // Speed of deleting in ms
   const pauseTime = 2000; // Pause before switching text
@@ -53,7 +53,8 @@ const HeroSection = () => {
           Welcome to my 
           <span className="typing-effect"> {displayedText}</span>
         </h1>
-        <p>This is still a work in progress.</p>
+        <p>This is still a work in progress. Currently working on 
+          <a href="https://ognegchat.netlify.app" target="_blank" rel="noreferrer"> GenChat</a></p>
       </div>
     </section>
     </div>
